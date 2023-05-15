fc8a2aca7e432c0c8ca9f1c885faa924cabdc7c4
Francisco Javier Alarza <fjalarza@viewnext.com>
2023-05-15 18:35:54 +0200

    Initial Commit

---
+++ FakeFlix/FakeFlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeFlixApp.swift
+//  FakeFlix
+//
+//  Created by Francisco Javier Alarza Sanchez on 15/5/23.
+//
+
+import SwiftUI
+
+@main
+struct FakeFlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
